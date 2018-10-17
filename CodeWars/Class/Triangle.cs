53a71a70f544e6bbc026469c288363225a7bc196
[syn]the <43966277+llsynthell@users.noreply.github.com>
2018-10-17 06:46:26 -0700

    Added challenge comment requirement.
    Updated the Triangle class to include a summary of the requirements from the customer.

--- CodeWars/Class/Triangle.cs
+++ CodeWars/Class/Triangle.cs
@@ -1,6 +1,7 @@
 ﻿public class Triangle
 {
     // Checks to see if 3 sides' lengths could create a triangle
+    // Assume that the customer input cannot be negatives
     public static bool IsTriangleWithPositiveSides(int a, int b, int c) => 
         a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && c + a > b;
 }
