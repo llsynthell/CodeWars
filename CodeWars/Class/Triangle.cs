3c6b3155a9f7fdc3d3a9fd382ed8c809d6a1ee26
[syn]the <43966277+llsynthell@users.noreply.github.com>
2018-10-17 06:40:55 -0700

    Clean up

--- CodeWars/Class/Triangle.cs
+++ CodeWars/Class/Triangle.cs
@@ -1,5 +1,6 @@
 ﻿public class Triangle
 {
+    // Checks to see if 3 sides' lengths could create a triangle
     public static bool IsTriangleWithPositiveSides(int a, int b, int c) => 
         a > 0 && b > 0 && c > 0 && a + b > c && b + c > a && c + a > b;
 }
